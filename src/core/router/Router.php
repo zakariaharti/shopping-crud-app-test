aa0c0ad86e769be781ba2f6e9f431dfec3dad7f7
zakariaharti <zakariya.developer@gmail.com>
2019-03-10 22:40:37 +0100

    add docs

--- src/core/router/Router.php
+++ src/core/router/Router.php
@@ -27,11 +27,11 @@ class Router
        }
      });
 
-     // Fetch method and URI from somewhere
+     
      $httpMethod = $_SERVER['REQUEST_METHOD'];
      $uri = $_SERVER['REQUEST_URI'];
 
-     
+
      if (false !== $pos = strpos($uri, '?')) {
          $uri = substr($uri, 0, $pos);
      }
